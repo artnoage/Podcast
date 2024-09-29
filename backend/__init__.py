8b00edb7e5fde631e65e7d379b73fb0caaa9f85b
artnoage <artnoage@gmail.com>
2024-09-29 17:51:35 +0200

    will see

--- backend/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This package contains the backend components of the podcast generation system.
-
-It includes modules for API endpoints, database interactions, and other server-side functionalities.
-"""
